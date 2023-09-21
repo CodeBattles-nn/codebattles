315271f0a97f61a61702a9d804023f522933a158
Doctorixx <61980858+Windows-up@users.noreply.github.com>
2023-09-21 15:24:45 +0300

    Delete unused env variable (CHECK_SERVER)

--- env.py
+++ env.py
@@ -1,19 +1,16 @@
 import os
 
-
-CHECK_SERVER = None
 DB_HOST = None
 DB_USERNAME = None
 DB_PASSWORD = None
 DB_NAME = None
 CHECKER_PORT = None
 
+
 def init():
-    global CHECK_SERVER, DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME, CHECKER_PORT
-    CHECK_SERVER = os.environ.get("CHECK_SERVER")
+    global DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME, CHECKER_PORT
     DB_HOST = os.environ.get("DB_HOST")
     DB_USERNAME = os.environ.get("DB_USERNAME")
     DB_PASSWORD = os.environ.get("DB_PASSWORD")
     DB_NAME = os.environ.get("DB_NAME")
     CHECKER_PORT = os.environ.get("CHECKER_PORT")
-
