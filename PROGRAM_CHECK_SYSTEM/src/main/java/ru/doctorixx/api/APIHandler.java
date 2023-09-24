ab98de5cf7c0d3192824e3fee4fb8f4a4deefafe
Doctorixx <61980858+Windows-up@users.noreply.github.com>
2023-09-24 19:05:34 +0300

    Running tests with thread pool

--- PROGRAM_CHECK_SYSTEM/src/main/java/ru/doctorixx/api/APIHandler.java
+++ PROGRAM_CHECK_SYSTEM/src/main/java/ru/doctorixx/api/APIHandler.java
@@ -16,61 +16,59 @@ import ru.doctorixx.core.RunManager;
 import ru.doctorixx.core.structures.ProgramResult;
 
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class APIHandler implements Handler {
 
-    private static Logger log = Logger.getLogger(APIHandler.class.getName());
+    private static final Logger log = Logger.getLogger(APIHandler.class.getName());
 
-
-    public static final MediaType JSON
-            = MediaType.parse("application/json; charset=utf-8");
-
+    public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
 
     private final ExecutorsFactory factory = new ExecutorsFactory();
 
+    private final ExecutorService threadPool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 
     @Override
     public void handle(@NotNull Context context) {
 
         APIRequest request = context.bodyAsClass(APIRequest.class);
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    ExecutionManager executionManager = new ExecutionManager(factory.get(request.compiler()), "", request.source());
-                    RunManager runManager = new RunManager(executionManager);
+        Runnable task = () -> {
+            try {
+                ExecutionManager executionManager = new ExecutionManager(factory.get(request.compiler()), "", request.source());
+                RunManager runManager = new RunManager(executionManager);
 
-                    List<ProgramResult> results = runManager.test(request.tests());
-                    System.out.println();
+                List<ProgramResult> results = runManager.test(request.tests());
+                System.out.println();
 
-                    for (ProgramResult res : results) {
-                        System.out.println(res.msg() + " " + res.time() + "ms");
-                    }
-
-
-                    System.out.println(runManager.getSuccessTestCount() + "/" + runManager.getTestCount());
-
-                    OkHttpClient client = new OkHttpClient();
-
-                    ObjectMapper objectMapper = new ObjectMapper();
-
-                    ApiResponse response = new ApiResponse(results, request.meta());
-
-                    RequestBody requestBody = RequestBody.create(JSON, objectMapper.writeValueAsString(response));
-                    Request senderRequest = new Request.Builder()
-                            .url(Env.get(Env.EnvVars.SERVER_ENDPOINT))
-                            .post(requestBody)
-                            .build();
-
-                    client.newCall(senderRequest).execute();
-                } catch (Exception e) {
-                    log.log(Level.INFO, e.getMessage(), e);
+                for (ProgramResult res : results) {
+                    System.out.println(res.msg() + " " + res.time() + "ms");
                 }
+
+
+                System.out.println(runManager.getSuccessTestCount() + "/" + runManager.getTestCount());
+
+                OkHttpClient client = new OkHttpClient();
+                ObjectMapper objectMapper = new ObjectMapper();
+
+                ApiResponse response = new ApiResponse(results, request.meta());
+                RequestBody requestBody = RequestBody.create(JSON, objectMapper.writeValueAsString(response));
+
+                Request senderRequest = new Request.Builder()
+                        .url(Env.get(Env.EnvVars.SERVER_ENDPOINT))
+                        .post(requestBody)
+                        .build();
+
+                client.newCall(senderRequest).execute();
+            } catch (Exception e) {
+                log.log(Level.INFO, e.getMessage(), e);
             }
-        }).start();
+        };
+
+        threadPool.execute(task);
 
         context.result("OK");
     }
