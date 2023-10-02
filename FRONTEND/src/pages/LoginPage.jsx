19869c0b86a5ae178f62f5acb51b5c3c144231ad
doctorixx <jebpip2008@gmail.com>
2023-10-02 16:37:45 +0300

    Test login

--- FRONTEND/src/pages/LoginPage.jsx
+++ FRONTEND/src/pages/LoginPage.jsx
@@ -1,29 +1,59 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from "axios";
 
 const LoginPage = (props) => {
+    const [id, setId] = useState(0);
+    const [login, setLogin] = useState();
+    const [passsword, setPasssword] = useState();
+
+    const [errorMsg, setErrorMsg] = useState("");
+
+
+    const onSend = async () => {
+        await axios.post('http://localhost:5000/api/login',
+            {id: id, login: login, password: passsword}).then(
+            (r) => {
+                setErrorMsg("Успешный вход")
+
+                axios.get("http://localhost:5000/api/problems").then(
+                    (r) => console.log(r.data.response)
+                )
+
+            }
+        ).catch(r => setErrorMsg("Неверные данные"));
+    };
+
     return (
         <div className="container" style={{"background-color": "#ffe0b2"}}>
             <div className="row" style={{"margin-top": "25%"}}>
                 <div className="col-2"></div>
                 <div className="col-8 bg-light p-5" style={{"border-radius": "15px"}}>
-                    <h4 className="text-danger">Error text</h4>
-                    <form action="/" method="post">
+                    <h4 className="text-danger">{errorMsg}</h4>
+                    <form>
                         <div className="form-group">
                             <label htmlFor="exampleInputEmail1">ID соревнования</label>
                             <input className="form-control" name="id" aria-describedby="emailHelp"
-                                   placeholder="Введите id"/>
+                                   placeholder="Введите id" onChange={
+                                (e) => setId(e.target.value)
+                            }/>
                         </div>
                         <div className="form-group">
                             <label htmlFor="exampleInputEmail1">Логин</label>
                             <input className="form-control" name="login" aria-describedby="emailHelp"
-                                   placeholder="Введите логин"/>
+                                   placeholder="Введите логин" onChange={
+                                (e) => setLogin(e.target.value)
+                            }/>
                         </div>
                         <div className="form-group">
                             <label htmlFor="exampleInputPassword1">Пароль</label>
                             <input type="password" className="form-control" name="password"
-                                   placeholder="Введите пароль"/>
+                                   placeholder="Введите пароль" onChange={
+                                (e) => setPasssword(e.target.value)
+                            }/>
                         </div>
-                        <button type="submit" className="btn btn-primary text-white">Войти</button>
+
+
+                        <button type="button" onClick={() => onSend()} className="btn btn-primary text-white">Войти</button>
                     </form>
                 </div>
                 <div className="col-2"></div>
