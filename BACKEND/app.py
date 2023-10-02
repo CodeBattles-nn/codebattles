c6cb2050ff347c5dbbe3c44ac7a23f2c680dfc79
Doctorixx <61980858+Windows-up@users.noreply.github.com>
2023-10-02 21:14:10 +0300

    Fix CORS

--- BACKEND/app.py
+++ BACKEND/app.py
@@ -1,5 +1,23 @@
-from flask import Flask
-from flask_cors import CORS
+from flask import Flask, request, make_response
 
 app = Flask(__name__)
-CORS(app)
+
+
+@app.after_request
+def after_request_func(response):
+    origin = request.headers.get('Origin')
+    if request.method == 'OPTIONS':
+        response = make_response()
+        response.headers.add('Access-Control-Allow-Credentials', 'true')
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
+        response.headers.add('Access-Control-Allow-Headers', 'x-csrf-token')
+        response.headers.add('Access-Control-Allow-Methods',
+                             'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+        if origin:
+            response.headers.add('Access-Control-Allow-Origin', origin)
+    else:
+        response.headers.add('Access-Control-Allow-Credentials', 'true')
+        if origin:
+            response.headers.add('Access-Control-Allow-Origin', origin)
+
+    return response
