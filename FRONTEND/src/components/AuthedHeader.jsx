c31d7977c287f7ff78498a5ce2a97c0eff866042
Doctorixx <61980858+Windows-up@users.noreply.github.com>
2023-10-18 17:54:05 +0300

    Logo and logout btn

--- FRONTEND/src/components/AuthedHeader.jsx
+++ FRONTEND/src/components/AuthedHeader.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
-import {Link} from "react-router-dom";
+import React, {useState} from 'react';
+import {Link, useNavigate} from "react-router-dom";
+import getApiAddress from "../utils/api";
 
 const AuthedHeader = (props) => {
-    return <nav className="navbar navbar-expand-md navbar-dark bg-dark" style={{height:"56px"}}>
+
+
+    const nav = useNavigate();
+
+    const [logoutProcessing, setLogoutProcessing] = useState(false);
+
+    const onLogoutBtnClicked = () => {
+        setLogoutProcessing(true)
+        fetch(getApiAddress() + "/api/logout")
+            .then(() => nav("/login"))
+            .finally(() => setLogoutProcessing(false))
+    }
+
+    return <nav className="navbar navbar-expand-md navbar-dark bg-dark" style={{height: "56px"}}>
         <div className="container-fluid">
-            <a href="/" className="navbar-brand" style={{"color": "red"}}>CodeBattles</a>
+            <Link to="/problems" className="navbar-brand" style={{"color": "red"}}>
+                <img src="/logo200.png" width="30" height="30"
+                     className="d-inline-block align-top mr-1" alt=""/>
+                {/*CodeBattles*/}
+            </Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
                     aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
@@ -25,7 +43,9 @@ const AuthedHeader = (props) => {
                     <li className="nav-item">
                         <Link className="nav-link" to="/statuses">Помощь</Link>
                     </li>
-                    <button className="btn btn-danger ml-2">Выход</button>
+                    <button className="btn btn-danger ml-2" onClick={onLogoutBtnClicked}
+                            disabled={logoutProcessing}>Выход
+                    </button>
                 </ul>
             </div>
         </div>
