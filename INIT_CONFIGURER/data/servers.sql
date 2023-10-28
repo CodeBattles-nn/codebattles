bbf3168a882346b1c9fd1b0ebe4c0eb7d22fe5d0
Doctorixx <61980858+Windows-up@users.noreply.github.com>
2023-10-28 20:30:50 +0300

    INIT_CONFIGURER: Added sqls and more

---
+++ INIT_CONFIGURER/data/servers.sql
@@ -0,0 +1,2 @@
+INSERT INTO servers (name, lang_name, address) VALUES ('Python 3.8', 'python', 'checker-python');
+INSERT INTO servers (name, lang_name, address) VALUES ('Java 17', 'java', 'checker-java');
