47dd092d401f1e478c3cb1be2552a07b314651bb
Doctorixx <61980858+Windows-up@users.noreply.github.com>
2023-11-06 17:41:57 +0300

    BACKEND: Admin configuration

--- BACKEND/src/env.py
+++ BACKEND/src/env.py
@@ -6,15 +6,19 @@ DB_PASSWORD = None
 DB_NAME = None
 CHECKER_PORT = None
 HASH_SALT = None
-REDIS_HOST = "redis"
+REDIS_HOST = None
+ADMIN_LOGIN = None
+ADMIN_PASSWORD = None
 
 
 def init():
-    global DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME, CHECKER_PORT, HASH_SALT, REDIS_HOST
+    global DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME, CHECKER_PORT, HASH_SALT, REDIS_HOST, ADMIN_LOGIN, ADMIN_PASSWORD
     DB_HOST = os.environ.get("DB_HOST")
     DB_USERNAME = os.environ.get("DB_USERNAME")
     DB_PASSWORD = os.environ.get("DB_PASSWORD")
     DB_NAME = os.environ.get("DB_NAME")
     CHECKER_PORT = os.environ.get("CHECKER_PORT")
     HASH_SALT = os.environ.get("HASH_SALT")
-    # REDIS_HOST = os.environ.get("REDIS_HOST")
+    REDIS_HOST = os.environ.get("REDIS_HOST")
+    ADMIN_LOGIN = os.environ.get("ADMIN_LOGIN")
+    ADMIN_PASSWORD = os.environ.get("ADMIN_PASSWORD")
