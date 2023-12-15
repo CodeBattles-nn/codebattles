2508abf89425ca5f10ddc3c50ce06664289ca503
Doctorixx <61980858+Windows-up@users.noreply.github.com>
2023-12-15 21:33:12 +0300

    BACKEND: Teacher can add problems

--- BACKEND/src/web/teacher_api/problems.py
+++ BACKEND/src/web/teacher_api/problems.py
@@ -46,3 +46,35 @@ def get_problems_byid_api_route(problem_id):
     }
 
     return out_data
+
+
+@app.route("/api/teacher/problems/add", methods=['POST'])
+def teacher_list_problems_add():
+    connection = get_connection()
+    cursor = connection.cursor()
+
+    build = request.json['build']
+    try:
+        build_json = json.loads(build)
+        build_json['tests'] = json.dumps(build_json['tests'])
+        build_json['examples'] = json.dumps(build_json['examples'])
+    except Exception as e:
+        print("Maybe Json parse exception \n" + str(e))
+        return "Not json (404 ERR)", 400
+
+    print(build)
+    print(build_json)
+
+    cursor.execute(
+        """INSERT INTO problems (name, description, "in", out, examples, tests) VALUES (%s, %s, %s, %s, %s, %s) """,
+        (build_json['name'],
+         build_json['description'],
+         build_json['in'],
+         build_json['out'],
+         build_json['examples'],
+         build_json['tests'],)
+    )
+
+    connection.commit()
+
+    return {"success": True}
