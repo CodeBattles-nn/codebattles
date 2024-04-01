6d3cd641fc41cd7d12119e24f22463b765df6e31
Doctorixx <61980858+Windows-up@users.noreply.github.com>
2024-04-01 19:11:00 +0300

    Frontend: delete navigation

--- FRONTEND/src/components/AuthedHeader.jsx
+++ FRONTEND/src/components/AuthedHeader.jsx
@@ -25,51 +25,6 @@ const AuthedHeader = () => {
     }
 
     return <nav className="navbar navbar-expand-md navbar-dark bg-dark" style={{minHeight: "56px"}}>
-        <div className="container-fluid">
-            <Link to="/problems" className="navbar-brand" style={{"color": "red"}}>
-                <img src="/logo200.png" width="30" height="30"
-                     className="d-inline-block align-top mr-1" alt="Логотип"/>
-                {/*CodeBattles*/}
-            </Link>
-            <button className="navbar-toggler" type="button" data-toggle="collapse"
-                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                    aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav mr-auto">
-                    <li className="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
-                        <Link className="nav-link" to="/problems">Задачи</Link>
-                    </li>
-                    <li className="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
-                        <Link className="nav-link" to="/stats">Рейтинг</Link>
-                    </li>
-                    <li className="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
-                        <Link className="nav-link" to="/sends">Посылки</Link>
-                    </li>
-                </ul>
-                <ul className="navbar-nav  my-2 my-md-0">
-                    <div className="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                        <a className="dropdown-item" href="#">Action</a>
-                        <a className="dropdown-item" href="#">Another action</a>
-                        <a className="dropdown-item" href="#">Something else here</a>
-                    </div>
-                    <li className="nav-item mr-2" data-toggle="collapse" data-target=".navbar-collapse.show">
-                        <Link className="nav-link" to="/statuses">Помощь</Link>
-                    </li>
-                    <div data-toggle="collapse" data-target=".navbar-collapse.show">
-                        <button className="btn btn-danger" onClick={onLogoutBtnClicked}
-                                disabled={logoutProcessing}
-
-                        >Выход
-                        </button>
-                    </div>
-
-                    <ChangeThemeButton />
-
-                </ul>
-            </div>
-        </div>
     </nav>
 };
 
