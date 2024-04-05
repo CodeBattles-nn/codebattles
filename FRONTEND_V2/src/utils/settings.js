6f26ab34e92d849ddfe6bf2e62c6b9d88f475b1a
Doctorixx <61980858+Windows-up@users.noreply.github.com>
2024-04-05 16:07:19 +0300

    Frontend_v2: change url selection method

--- FRONTEND_V2/src/utils/settings.js
+++ FRONTEND_V2/src/utils/settings.js
@@ -4,5 +4,6 @@ axios.defaults.withCredentials = true
 
 const isProd = import.meta.env.VITE_ENV === "production";
 if (!isProd) {
-    axios.defaults.baseURL = 'http://localhost:2500';
+    axios.defaults.baseURL =`${document.location.protocol}//${document.location.hostname}:${document.location.port}`
 }
+
