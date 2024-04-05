97bbbec92ce6f142a41374b44cd7d6dc84501741
Doctorixx <61980858+Windows-up@users.noreply.github.com>
2024-04-05 15:31:46 +0300

    Frontend_v2: fix url

--- FRONTEND_V2/src/utils/settings.js
+++ FRONTEND_V2/src/utils/settings.js
@@ -3,8 +3,6 @@ import axios from "axios";
 axios.defaults.withCredentials = true
 
 const isProd = import.meta.env.VITE_ENV === "production";
-if (isProd) {
-    axios.defaults.baseURL = `${location.protocol}//${location.host}`
-} else {
+if (!isProd) {
     axios.defaults.baseURL = 'http://localhost:2500';
 }
