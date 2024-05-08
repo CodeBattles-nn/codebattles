397183df0f06d07d5dd83f51a81281f79dc0727c
Doctorixx <61980858+windows-up@users.noreply.github.com>
2024-05-08 17:07:12 +0300

    backend: add local captcha

--- BACKEND/src/env.py
+++ BACKEND/src/env.py
@@ -9,11 +9,14 @@ HASH_SALT = None
 REDIS_HOST = None
 ADMIN_LOGIN = None
 ADMIN_PASSWORD = None
+REQUIRE_CAPTCHA = None
 
 
 def init():
     global DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME, \
-        CHECKER_PORT, HASH_SALT, REDIS_HOST, ADMIN_LOGIN, ADMIN_PASSWORD
+        CHECKER_PORT, HASH_SALT, REDIS_HOST, ADMIN_LOGIN, ADMIN_PASSWORD, \
+        REQUIRE_CAPTCHA
+
     DB_HOST = os.environ.get("DB_HOST")
     DB_USERNAME = os.environ.get("DB_USERNAME")
     DB_PASSWORD = os.environ.get("DB_PASSWORD")
@@ -23,3 +26,9 @@ def init():
     REDIS_HOST = os.environ.get("REDIS_HOST")
     ADMIN_LOGIN = os.environ.get("ADMIN_LOGIN")
     ADMIN_PASSWORD = os.environ.get("ADMIN_PASSWORD")
+    REQUIRE_CAPTCHA = os.environ.get("REQUIRE_CAPTCHA")
+
+    if REQUIRE_CAPTCHA == "true":
+        REQUIRE_CAPTCHA = True
+    else:
+        REQUIRE_CAPTCHA = False
--- BACKEND/src/web/teacher_api/auth.py
+++ BACKEND/src/web/teacher_api/auth.py
@@ -1,20 +1,47 @@
-from flask import request, make_response
+from flask import request, make_response, jsonify
 from psycopg2.extras import RealDictCursor
 
+import env
 from app import app
 from database import get_connection
+from services import captcha_service
+
+
+@app.route("/api/teacher/auth")
+def teacher_auth_captcha():
+    captcha = captcha_service.generate_with_validation()
+    return {
+        "base64string": captcha.base64image,
+        "validate": captcha.sha256_hash,
+    }
 
 
 @app.route("/api/teacher/auth", methods=['POST'])
 def teacher_auth_post():
     login, password = request.json['login'], request.json['password']
 
+    if env.REQUIRE_CAPTCHA:
+        base64image, captchaUserInput, captchaValidate = request.json[
+            'base64image'], request.json['captchaUserInput'], request.json[
+            'captchaValidate'],
+
+        success_captcha = captcha_service.validate(
+            base64image,
+            captchaUserInput,
+            captchaValidate
+        )
+
+        if not success_captcha:
+            return make_response({"success": False, "use_redirect": False}, 403)
+
+
+
     connection = get_connection()
     cursor = connection.cursor(cursor_factory=RealDictCursor)
 
     cursor.execute(f"""
     SELECT id from globalusers
-WHERE role = 'TEACHER' 
+WHERE role = 'TEACHER'
   AND password = '{password}'
   AND login = '{login}'
 """)
