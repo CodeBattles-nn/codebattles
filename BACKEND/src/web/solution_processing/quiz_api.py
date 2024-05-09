881ae8babcc3ef0be5e3f985daec3fa2dc02e569
Doctorixx <61980858+windows-up@users.noreply.github.com>
2024-05-09 16:52:07 +0300

    backend: quiz processing endpoint

--- BACKEND/src/web/solution_processing/quiz_api.py
+++ BACKEND/src/web/solution_processing/quiz_api.py
@@ -1,98 +1,85 @@
-import json
-import string
 import datetime
+import json
 
-import requests
-from flask import redirect, request
+from flask import request
+from psycopg2.extras import RealDictCursor
 
 from app import app
 from database import get_connection
-from decorators import login_required, get_user_id, redis_conn
-import env
+from decorators import login_required, get_user_id
 
 
-@app.route("/send/quiz", methods=['POST'])
+@app.route("/api/send/quiz", methods=['POST'])
 @login_required
 @get_user_id
 def send_quiz_solution(user_id, uid):
     connection = get_connection()
     cur = connection.cursor()
+    cur_dict = connection.cursor(cursor_factory=RealDictCursor)
 
-    cur.execute("SELECT * FROM champs WHERE id = %s", (str(user_id),))
+    answers: dict
+    problem, answers = request.json['problem'], request.json['answers']
+
+    cur.execute(f"SELECT {problem.lower()} FROM champs WHERE id = %s",
+                (str(user_id),))
 
     fetch = cur.fetchone()  # Can be None
-    problems_ids_temp = fetch[3:]
-    problems_ids = []
+    problem_id = fetch[0]
+    print(problem_id)
 
-    sql = "SELECT * FROM problems WHERE id = -1 "
+    cur_dict.execute(f"SELECT tests, name FROM problems WHERE id = %s",
+                     (problem_id,))
+    fetch = cur_dict.fetchone()
+    tests = fetch['tests']
+    problem_name = fetch['name']
 
-    strs = string.ascii_uppercase
+    tests_dict = json.loads(tests)
+    questionById = {}
+    qustionsCount = 0
+    for question in tests_dict['questions']:
+        questionById[question['id']] = question
+        qustionsCount += 1
+    print(questionById)
 
-    for i in problems_ids_temp:
-        if i is not None:
-            problems_ids.append(i)
-            sql += "OR id = %s "
+    points = 0
+    for answer_id, answer in answers.items():
+        current_answer = answer[0]
+        print(questionById)
+        correct_answer = questionById[int(answer_id)]['correct_answers'][0]
+        print(correct_answer)
+        if current_answer == correct_answer:
+            points += 1
 
-    cur.execute(sql, tuple(problems_ids))
-
-    x = list(cur.fetchall())
-
-    problem_ = None
-    problem_letter_form = request.form['problem']
-
-    for i in x:
-        _id = i[0]
-        problem_letter = strs[problems_ids.index(_id)]
-        if problem_letter_form == problem_letter:
-            problem_ = i
-
-    tests = problem_[5]
-    tests = json.loads(tests)
-    tests = list(map(lambda z: {"in": z[0], "out": z[1]}, tests))
-
-    print(problem_)
-
-    f_lang = request.form['cars']
-    f_code = request.form['src']
+    totalPoints = int(points / qustionsCount * 100)
 
     cur.execute(
         f'''
         INSERT INTO champSends_{user_id}
-        (problem_name, problem_id, user_id, send_time, state, program, problem_letter, lang)
+        (problem_name, problem_id, user_id, send_time, state, program, problem_letter, score)
         VALUES(%s, %s, %s, %s, %s, %s, %s, %s);
         ''',
         (
-            problem_[1], problem_[0], uid, datetime.datetime.now(), "Тестируется", f_code,
-            problem_letter_form,
-            f_lang)
+            problem_name,
+            problem_id,
+            uid,
+            datetime.datetime.now(),
+            "Подсчитано",
+            json.dumps(answers),
+            problem,
+            totalPoints
+
+        )
     )
-    cur.execute(f"SELECT currval(pg_get_serial_sequence('champSends_{user_id}','id'));")
 
-    inserted_id = cur.fetchone()[0]
-
-    meta = {
-        "champ_id": user_id,
-        "user_id": uid,
-        "problem": request.form['problem'],
-        "id": inserted_id,
-    }
-
-    data = {
-        "meta": json.dumps(meta),
-        "source": request.form['src'],
-        "compiler": request.form['cars'],
-        "tests": tests,
-    }
+    cur.execute(
+        f"""
+        UPDATE champUsers_{user_id} SET {problem.lower()} = {totalPoints}
+        WHERE id= {user_id} AND (
+        {problem.lower()} < {points}
+        OR
+        {problem.lower()} IS NULL
+        )""")
 
     connection.commit()
 
-    print(cur.fetchone())
-
-    cur.execute(f"SELECT address FROM servers WHERE id = {request.form['cars']}")
-
-    server_addr = cur.fetchone()[0]
-
-    print()
-
-    return redirect("/sends")
-
+    return {"ok": "ok"}
