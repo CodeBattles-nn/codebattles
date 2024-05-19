9d1b6363b09994d86344397d0bb9c7c6b56fe05e
Doctorixx <61980858+Windows-up@users.noreply.github.com>
2024-05-19 11:12:52 +0300

    frontend_v2: fix api port

--- FRONTEND_V2/src/utils/settings.js
+++ FRONTEND_V2/src/utils/settings.js
@@ -4,6 +4,6 @@ axios.defaults.withCredentials = true
 
 const isProd = import.meta.env.VITE_ENV === "production";
 if (!isProd) {
-    axios.defaults.baseURL =`${document.location.protocol}//${document.location.hostname}:2500`
+    // axios.defaults.baseURL =`${document.location.protocol}//${document.location.hostname}:2500`
 }
 
