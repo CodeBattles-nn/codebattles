a2381ecbedf75aca4f178dd335c8d77f0cb0820d
Doctorixx <61980858+Windows-up@users.noreply.github.com>
2024-12-31 18:39:10 +0300

    frontend_2: improve ui

---
+++ FRONTEND_V2/src/components/BreadcrumbsElement.jsx
@@ -0,0 +1,23 @@
+import Card from "./bootstrap/Card.jsx";
+import {Link} from "react-router-dom";
+
+const BreadcrumbsElement = ({name, url, active}) => {
+
+    const isActiveClass = active ? ("active") : ("")
+
+    return (
+
+        url ? (
+            <li className={"breadcrumb-item " + isActiveClass}>
+                <Link to={url}>{name}</Link>
+            </li>
+        ) : (
+            <li className={"breadcrumb-item " + isActiveClass}>
+                {name}
+            </li>
+        )
+
+    );
+};
+
+export default BreadcrumbsElement;
