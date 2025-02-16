b2578fb44b50988a26f04d1417e6ac1c97e1a5cf
Doctorixx <61980858+Windows-up@users.noreply.github.com>
2025-02-16 16:31:58 +0300

    backend_v2: get users

--- BACKEND_V2/src/main/kotlin/ru/codebattles/backend/web/controllers/UsersController.kt
+++ BACKEND_V2/src/main/kotlin/ru/codebattles/backend/web/controllers/UsersController.kt
@@ -1,5 +1,6 @@
 package ru.codebattles.backend.web.controllers
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -10,11 +11,18 @@ import java.util.*
 
 @RestController
 @RequestMapping("/api/users")
+@SecurityRequirement(name = "JWT")
 class UsersController(
     val userRepository: UserRepository,
 ) {
     @GetMapping("me")
     fun getProfile(@AuthenticationPrincipal user: User): Optional<User> {
         return userRepository.findById(user.id!!)
+    }
+
+
+    @GetMapping
+    fun getAll(@AuthenticationPrincipal user: User): MutableList<User> {
+        return userRepository.findAll()
     }
 }
