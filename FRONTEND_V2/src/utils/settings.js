42a28fcc5a1b7e87e2951d9e1063a79ae51fd5ee
Doctorixx <61980858+Windows-up@users.noreply.github.com>
2025-03-02 22:52:49 +0300

    docker ready

--- FRONTEND_V2/src/utils/settings.js
+++ FRONTEND_V2/src/utils/settings.js
@@ -1,6 +1,13 @@
 import axios from "axios";
 
-const productionBuild = import.meta.env.VITE_ENV === "production";
-if (!productionBuild) {
-    axios.defaults.baseURL = `${document.location.protocol}//${document.location.hostname}:${8080}`
-}
+// const productionBuild = import.meta.env.VITE_ENV === "production";
+// if (!productionBuild) {
+//     axios.defaults.baseURL = `${document.location.protocol}//${document.location.hostname}:${8080}`
+// }
+// const baseUrl = import.meta.env.VITE_API_ENDPOINT
+//
+// console.log("BASEURL: "+ baseUrl)
+//
+// if (baseUrl && baseUrl !== ""){
+//     axios.defaults.baseURL = baseUrl
+// }
