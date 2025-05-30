d406191cefec5b392b12aedb39427156964bb27e
doctorixx <61980858+windows-up@users.noreply.github.com>
2025-05-30 22:13:46 +0300

    frontend: competition status (ended, started, etc)

--- FRONTEND_V2/src/components/CompetitionCard.jsx
+++ FRONTEND_V2/src/components/CompetitionCard.jsx
@@ -1,16 +1,36 @@
 import React from 'react';
 import Card from "./bootstrap/Card.jsx";
 import PropTypes from "prop-types";
+import {
+    competitionStatusesCssColor,
+    competitionStatusesTranslate,
+    getCompetitionStatusByDates
+} from "../utils/competitionStatuses.js";
 
-export const CompetitionCard = ({name, description, children, id}) => {
+export const CompetitionCard = ({name, description, children, id, startedAt, endedAt}) => {
+    let status = getCompetitionStatusByDates(startedAt,endedAt)
+
+    if (startedAt === undefined || endedAt === undefined) {
+        status = undefined
+    }
+
+
+    const statusCss = competitionStatusesCssColor[status]
+    const statusText = competitionStatusesTranslate[status]
+
+
     return (
         <Card>
-            <h5><span className="badge text-bg-primary">Идет</span></h5>
+            <h5><span className={`badge ${statusCss}`}>{statusText}</span></h5>
             <div className="d-flex align-items-start gap-2">
                 <h2 className="text">{name}</h2><small className="text-muted">id={id || "0000"}</small>
             </div>
             <p className="text-muted">{description}</p>
 
+            <small className="text-muted"><i className="bi bi-clock me-1"></i>Начало: {startedAt}</small>
+            <br />
+            <small className="text-muted"><i className="bi bi-clock me-1"></i>Конец: {endedAt} </small>
+            <p className="mb-2"/>
             {children}
         </Card>
     );
@@ -21,4 +41,6 @@ CompetitionCard.propTypes = {
     description: PropTypes.string.isRequired,
     children: PropTypes.node,
     id: PropTypes.string,
+    startedAt: PropTypes.string,
+    endedAt: PropTypes.string,
 }
--- FRONTEND_V2/src/utils/settings.js
+++ FRONTEND_V2/src/utils/settings.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // const productionBuild = import.meta.env.VITE_ENV === "production";
 // if (!productionBuild) {
-//     axios.defaults.baseURL = `${document.location.protocol}//${document.location.hostname}:${8080}`
+    axios.defaults.baseURL = `https://codebattles.school30nn.ru`
 // }
 // const baseUrl = import.meta.env.VITE_API_ENDPOINT
 //
