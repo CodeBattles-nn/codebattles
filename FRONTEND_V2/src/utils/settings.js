b707c92d99cc0d271abeb7738d2f3b384142570b
doctorixx <61980858+windows-up@users.noreply.github.com>
2025-06-12 20:19:07 +0300

    fix frontend

--- FRONTEND_V2/src/utils/settings.js
+++ FRONTEND_V2/src/utils/settings.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 // const productionBuild = import.meta.env.VITE_ENV === "production";
 // if (!productionBuild) {
-    axios.defaults.baseURL = `https://codebattles.school30nn.ru`
 // }
 // const baseUrl = import.meta.env.VITE_API_ENDPOINT
 //
