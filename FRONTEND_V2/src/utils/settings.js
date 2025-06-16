2224668779ec07b3a00b7cde9feb51ccb9cbfacf
doctorixx <61980858+windows-up@users.noreply.github.com>
2025-06-16 22:23:35 +0300

    fix settings.js

--- FRONTEND_V2/src/utils/settings.js
+++ FRONTEND_V2/src/utils/settings.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // const productionBuild = import.meta.env.VITE_ENV === "production";
 // if (!productionBuild) {
 
-axios.defaults.baseURL = "http://localhost:8080";
+// axios.defaults.baseURL = "http://localhost:8080";
 
 // }
 // const baseUrl = import.meta.env.VITE_API_ENDPOINT
