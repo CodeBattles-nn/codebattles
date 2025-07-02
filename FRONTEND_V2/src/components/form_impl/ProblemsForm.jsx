0c4012bba2f1bdfe0553f3c3f5aa6e2d4e7284ca
Doctorixx <61980858+Windows-up@users.noreply.github.com>
2025-07-02 15:15:04 +0300

    frontend: fix problem edit/create

--- FRONTEND_V2/src/components/form_impl/ProblemsForm.jsx
+++ FRONTEND_V2/src/components/form_impl/ProblemsForm.jsx
@@ -1,33 +1,31 @@
 import React from 'react';
-import {InputFormElement} from "../forms/InputFormElement.jsx";
+import { InputFormElement } from "../forms/InputFormElement.jsx";
 import PropTypes from "prop-types";
 import { useTranslation } from 'react-i18next';
 
-export const ProblemsForm = ({form, testsArray, examplesArray}) => {
+export const ProblemsForm = ({ form, testsArray, examplesArray }) => {
     const { t } = useTranslation();
     const {
         register,
-    } = form
+    } = form;
 
-    const {fields: testFields, append: appendTest, remove: removeTest} = testsArray
-    const {fields: exampleFields, append: appendExample, remove: removeExample} = examplesArray
+    const { fields: testFields, append: appendTest, remove: removeTest } = testsArray;
+    const { fields: exampleFields, append: appendExample, remove: removeExample } = examplesArray;
 
     return (
         <>
-
             <InputFormElement
                 displayName={t('adminProblems.name')}
                 name={"name"}
-                args={{required: t('adminProblems.nameRequired')}}
+                args={{ required: t('adminProblems.nameRequired') }}
             />
             <InputFormElement
                 displayName={t('adminProblems.description')}
                 name={"description"}
-                args={{required: t('adminProblems.descriptionRequired')}}
+                args={{ required: t('adminProblems.descriptionRequired') }}
             />
-            <InputFormElement displayName={t('adminProblems.inData')} name={"inData"}/>
-            <InputFormElement displayName={t('adminProblems.outData')} name={"outData"}/>
-
+            <InputFormElement displayName={t('adminProblems.inData')} name={"inData"} />
+            <InputFormElement displayName={t('adminProblems.outData')} name={"outData"} />
 
             {/* Tests Section */}
             <div className="mb-3">
@@ -35,28 +33,31 @@ export const ProblemsForm = ({form, testsArray, examplesArray}) => {
                 {testFields.map((item, index) => (
                     <div key={item.id} className="row mb-2">
                         <div className="col">
-                            <input
+                            <textarea
                                 className="form-control"
+                                rows={3}
                                 placeholder={t('adminProblems.testIn')}
                                 {...register(`tests.${index}.in`)}
                             />
                         </div>
                         <div className="col">
-                            <input
+                            <textarea
                                 className="form-control"
+                                rows={3}
                                 placeholder={t('adminProblems.testOut')}
                                 {...register(`tests.${index}.out`)}
                             />
                         </div>
                         <div className="col-auto">
                             <button type="button" className="btn btn-danger"
-                                    onClick={() => removeTest(index)}>{t('adminProblems.removeTest')}
+                                    onClick={() => removeTest(index)}>
+                                {t('adminProblems.removeTest')}
                             </button>
                         </div>
                     </div>
                 ))}
                 <button type="button" className="btn btn-secondary"
-                        onClick={() => appendTest({in: "", out: ""})}>
+                        onClick={() => appendTest({ in: "", out: "" })}>
                     {t('adminProblems.addTest')}
                 </button>
             </div>
@@ -67,28 +68,31 @@ export const ProblemsForm = ({form, testsArray, examplesArray}) => {
                 {exampleFields.map((item, index) => (
                     <div key={item.id} className="row mb-2">
                         <div className="col">
-                            <input
+                            <textarea
                                 className="form-control"
+                                rows={3}
                                 placeholder={t('adminProblems.exampleIn')}
                                 {...register(`examples.${index}.in`)}
                             />
                         </div>
                         <div className="col">
-                            <input
+                            <textarea
                                 className="form-control"
+                                rows={3}
                                 placeholder={t('adminProblems.exampleOut')}
                                 {...register(`examples.${index}.out`)}
                             />
                         </div>
                         <div className="col-auto">
                             <button type="button" className="btn btn-danger"
-                                    onClick={() => removeExample(index)}>{t('adminProblems.removeExample')}
+                                    onClick={() => removeExample(index)}>
+                                {t('adminProblems.removeExample')}
                             </button>
                         </div>
                     </div>
                 ))}
                 <button type="button" className="btn btn-secondary"
-                        onClick={() => appendExample({in: "", out: ""})}>
+                        onClick={() => appendExample({ in: "", out: "" })}>
                     {t('adminProblems.addExample')}
                 </button>
             </div>
@@ -99,11 +103,8 @@ export const ProblemsForm = ({form, testsArray, examplesArray}) => {
     );
 };
 
-
 ProblemsForm.propTypes = {
     form: PropTypes.object,
     testsArray: PropTypes.object,
     examplesArray: PropTypes.object,
-}
-
-
+};
